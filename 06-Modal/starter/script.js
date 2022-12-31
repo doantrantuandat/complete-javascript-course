927b07cbdfe478c65ee8be8bb45fee4508c63c98
DAT-PC\USER <doantrantuandat.98@gmail.com>
2022-12-31 15:56:37 +0700

    show modal project final

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,32 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+console.log(btnShowModal);
+console.log(modal);
+console.log(modal.classList);
+
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnShowModal.forEach(element => {
+  element.addEventListener('click', openModal);
+});
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
